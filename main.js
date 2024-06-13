d6cf0dfa06cac54ae4b6ef7ef4f3c2120a9a2db6
Romain <romain.tortosa@gmail.com>
2024-06-13 03:34:03 +0200

    les fonctions

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/**
+ * Variables
+ */
+
+
+
 var nomDuJoueur1 = "Torto";
 var ageDuJoueur1 = 31;
 var estUnHommeJoueur1 = true;
@@ -6,20 +12,44 @@ var nomDuJoueur2 = "Tarta";
 var ageDuJoueur2 = 22;
 var estUnHommeJoueur2 = false;
 
-console.log("Nom du joueur 1 : " + nomDuJoueur1);
-console.log("Age du joueur 1 : " + ageDuJoueur1);
-if(estUnHommeJoueur1 == true){
-    console.log("le joueur 1 est un homme");
-} else {
-    console.log("Le joueur 1 est une femme");
+
+
+/**
+ * Main
+ */
+
+afficherLesJoueur();
+comparerAgeDesDeuxJoueurs();
+
+/**
+ * Fonctions
+ */
+
+function afficherLesJoueur(){
+    console.log("Nom du joueur 1 : " + nomDuJoueur1);
+    console.log("Age du joueur 1 : " + ageDuJoueur1);
+    if(estUnHommeJoueur1 == true){
+        console.log("le joueur 1 est un homme");
+    } else {
+        console.log("Le joueur 1 est une femme");
+    }
+    
+    console.log("%s est le nom du joueur 2", nomDuJoueur2);
+    console.log("%d est l'age du joueur 2" , ageDuJoueur2);
+    if(estUnHommeJoueur2 == true){
+        console.log("le joueur 2 est un homme");
+    } else {
+        console.log("Le joueur 2 est une femme");
+    }
+    
 }
 
-
-
-console.log("%s est le nom du joueur 2", nomDuJoueur2);
-console.log("%d est l'age du joueur 2" , ageDuJoueur2);
-if(estUnHommeJoueur2 == true){
-    console.log("le joueur 2 est un homme");
-} else {
-    console.log("Le joueur 2 est une femme");
+function comparerAgeDesDeuxJoueurs(){
+    if(ageDuJoueur1 > ageDuJoueur2){
+        console.log("Le joueur 1 est le plus agé");
+    } else if (ageDuJoueur2 > ageDuJoueur1){
+        console.log("Le joueur 2 est le plus agé");
+    } else{
+        console.log ("Les joueurs ont le même age");
+    }
 }
