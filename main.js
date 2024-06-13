dde733d2aafd01031ac7bd8ae237a61a5e14a4e2
Romain <romain.tortosa@gmail.com>
2024-06-13 03:15:49 +0200

    variable

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log ("coucou");
+var nomDuJoueur1 = "Torto";
+var ageDuJoueur1 = 31;
+var estUnHommeJoueur1 = true;
+
+console.log("Nom du joueur 1 : " + nomDuJoueur1);
+console.log("Age du joueur 1 : " + ageDuJoueur1);
+if(estUnHommeJoueur1 == true){
+    console.log("le joueur 1 est un homme");
+} else {
+    console.log("Le joueur 1 est une femme");
+}
