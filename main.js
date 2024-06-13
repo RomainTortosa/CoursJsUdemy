614f7c39b0fb52985e6a979060edd142f43be360
Romain <romain.tortosa@gmail.com>
2024-06-13 03:23:02 +0200

    manipulation de chaine de caractères

--- main.js
+++ main.js
@@ -2,10 +2,24 @@ var nomDuJoueur1 = "Torto";
 var ageDuJoueur1 = 31;
 var estUnHommeJoueur1 = true;
 
+var nomDuJoueur2 = "Tarta";
+var ageDuJoueur2 = 22;
+var estUnHommeJoueur2 = false;
+
 console.log("Nom du joueur 1 : " + nomDuJoueur1);
 console.log("Age du joueur 1 : " + ageDuJoueur1);
 if(estUnHommeJoueur1 == true){
     console.log("le joueur 1 est un homme");
 } else {
     console.log("Le joueur 1 est une femme");
+}
+
+
+
+console.log("%s est le nom du joueur 2", nomDuJoueur2);
+console.log("%d est l'age du joueur 2" , ageDuJoueur2);
+if(estUnHommeJoueur2 == true){
+    console.log("le joueur 2 est un homme");
+} else {
+    console.log("Le joueur 2 est une femme");
 }
